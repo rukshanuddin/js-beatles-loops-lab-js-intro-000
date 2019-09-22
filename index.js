fee3812b02c41ad949dc923b7151bee178908648
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-09-22 00:28:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,14 +8,28 @@ function theBeatlesPlay(){
   }
   return theyPlay
 }
-function johnLennonFacts(){
+function johnLennonFacts([]){
   const facts = [
     "He was the last Beatle to learn to drive",
     "He was never a vegetarian",
     "He was a choir boy and boy scout",
     "He hated the sound of his own voice"
   ];
+  
   while (n < 4){
     console.log(`${facts[n]}!!!`)
   }
 }
+function iLoveTheBeatles(num) {
+	var i = 0;
+  const array[]
+	function incrementVariable() {
+		i = i + 1;
+    return i;
+	}
+
+  do {
+    array.push(`I love the Beatles!`);
+  } while (incrementVariable(num) < 15);
+}
+
