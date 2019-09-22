7917c86821983dee1799bed693b7b7dfe1f387ab
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-09-22 00:06:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+  const theyPlay = []
+  for (var i = 0; i <4; i++){
+    theyPlay.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return theyPlay
+}
